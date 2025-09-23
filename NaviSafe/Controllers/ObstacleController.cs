11090add49edf23f124bd95df4c7c8e11d4c4519
asklootz <askl@uia.no>
2025-09-24 00:56:10 +0200

    Adding last files

--- NaviSafe/Controllers/ObstacleController.cs
+++ NaviSafe/Controllers/ObstacleController.cs
@@ -1,6 +1,24 @@
-﻿namespace NaviSafe.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using NaviSafe.Models;
 
-public class ObstacleController
+namespace NaviSafe.Controllers;
+
+public class ObstacleController : Controller
 {
-    
+    [HttpGet]
+    public ActionResult DataForm()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult DataForm(ObstacleData obstacleData)
+    {
+        bool isDraft = false;
+        if (obstacleData.ObstacleDescription == null)
+        {
+            isDraft = true;
+        }
+        return View("Overview", obstacleData);
+    }
 }
