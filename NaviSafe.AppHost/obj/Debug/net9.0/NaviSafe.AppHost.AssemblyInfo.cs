264f0352bed97878d184b64f407a9cbde25ba43c
asklootz <askl@uia.no>
2025-11-03 18:55:43 +0100

    Auto stash before merge of "master" and "origin/master"

--- NaviSafe.AppHost/obj/Debug/net9.0/NaviSafe.AppHost.AssemblyInfo.cs
+++ NaviSafe.AppHost/obj/Debug/net9.0/NaviSafe.AppHost.AssemblyInfo.cs
@@ -25,7 +25,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NaviSafe.AppHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebc613f2a83864f7335934d5ba49defdd0d2b0eb")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+369f8b078c1ae4cffe9f5ce99efdf00f0e595907")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("NaviSafe.AppHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NaviSafe.AppHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
