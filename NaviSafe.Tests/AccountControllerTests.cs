7711b40ffb883878ee50d3f6dd78dae6a38026d3
Andre’ Abrahamsen <andreabrahamsen@icloud.com>
2025-11-15 13:48:23 +0100

    add test for AccountController login with non valid credentials

--- NaviSafe.Tests/AccountControllerTests.cs
+++ NaviSafe.Tests/AccountControllerTests.cs
@@ -27,4 +27,29 @@ public class AccountControllerTests
         var viewResult = Assert.IsType<ViewResult>(result);
         Assert.Same(model, viewResult.Model);
     }
+    
+    
+    [Fact]
+    public void LoginPostWillAddModelErrorWhenCredentialsAreNotValid()
+    {
+        var userStorage = new UserStorage();
+        var controller = new AccountController(userStorage);
+
+        var model = new LoginViewModel
+        {
+            Email = "blablabla@example.com",
+            Password = "NotCorrect123!"
+        };
+
+        var result = controller.Login(model, null);
+
+        var viewResult = Assert.IsType<ViewResult>(result);
+        Assert.Same(model, viewResult.Model);
+
+        Assert.False(controller.ModelState.IsValid);
+        Assert.True(controller.ModelState.ContainsKey(string.Empty));
+
+        var error = controller.ModelState[string.Empty]!.Errors.Single();
+        Assert.Equal("Invalid email or password.", error.ErrorMessage);
+    }
 }
