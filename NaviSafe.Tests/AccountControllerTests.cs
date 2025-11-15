e2249e8f918dc75594ce524d4579a3e5e1bc03c2
Andre’ Abrahamsen <andreabrahamsen@icloud.com>
2025-11-15 13:44:07 +0100

    add test for AccountController login with a not valid model

---
+++ NaviSafe.Tests/AccountControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NaviSafe.Controllers;
+using NaviSafe.Models;
+using NaviSafe.Services;
+using Xunit;
+
+namespace NaviSafe.Tests;
+
+public class AccountControllerTests
+{
+    [Fact]
+    public void LoginPostReturnsViewIfModelStateIsNotValid()
+    {
+        var userStorage = new UserStorage();
+        var controller = new AccountController(userStorage);
+
+        controller.ModelState.AddModelError("Email", "Required");
+
+        var model = new LoginViewModel
+        {
+            Email = "",
+            Password = ""
+        };
+
+        var result = controller.Login(model, null);
+
+        var viewResult = Assert.IsType<ViewResult>(result);
+        Assert.Same(model, viewResult.Model);
+    }
+}
