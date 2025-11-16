51ea0f9f3a6d211f0349bf97cc107559e53af6ae
asklootz <askl@uia.no>
2025-11-16 13:44:52 +0100

    First try to fix the error message "Resource file "*/.resx" cannot be found" for Mac-users

--- NaviSafe.AppHost/AppHost.cs
+++ NaviSafe.AppHost/AppHost.cs
@@ -4,7 +4,7 @@ var mariaContainer = builder.AddMySql("mariaContainer", null, 3307)
     .WithLifetime(ContainerLifetime.Persistent)
     .WithImage("mariadb:11.8")
     .WithContainerName("mariaContainer")
-    .WithDataBindMount(source: @"..\MariaDB\Data") //Code to create a bind mount to a local folder
+    .WithDataBindMount(source: "../MariaDB/Data") //Code to create a bind mount to a local folder
     .WithPhpMyAdmin()
     .WithOtlpExporter(); //Creates a phpMyAdmin container linked to the database container for easy management
 
@@ -22,9 +22,10 @@ builder.AddProject<Projects.NaviSafe>("navisafe")
 
 builder.AddDockerfile("naviSafe", "../", "NaviSafe/Dockerfile")
     .WithExternalHttpEndpoints()
-    //.WithReference(mariaDatabase)
-    //.WaitFor(mariaDatabase)
+    .WithReference(mariaDatabase)
+    .WaitFor(mariaDatabase)
     .WithHttpEndpoint(8080, 8080, "NaviSafe")
+    .WithHttpsEndpoint(8081, 8081, "NaviSafe")
     .WithOtlpExporter();
 
 builder.Build().Run();
